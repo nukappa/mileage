93fdd39bb8469b7d29553037a6db05ab57650814
nukappa <nikolaos.karaiskos@mdc-berlin.de>
2021-01-06 15:13:28 +0100

    finished

--- _utils.py
+++ _utils.py
@@ -1,10 +1,49 @@
+from calendar import monthrange
+import datetime
 import googlemaps
+import json
 from math import ceil
 import numpy as np
+import subprocess
+import xlsxwriter
+import yaml
 
-def read_gmaps_key(file):
+def read_months(filename):
+    '''Read list of months from disk and return a list'''
+    with open(filename, 'r') as f:
+        months = [line.strip() for line in f]
+    return months
+
+def read_database(filename):
+    '''Read local database of addresses from disk and return it as dictionary'''
+    db = json.load(open(filename, 'r'))
+    return db
+
+def read_month_data(filename):
+    '''Read the month data from disk and return it as dictionary'''
+    with open(filename, 'r') as stream:
+        try:
+            month_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return month_data
+
+def write_month_data(route_list, month_data, filename):
+    all_kms = sum(calculate_kms(route_list))
+    month_data['new_km_stand'] = month_data['last_km_stand'] + all_kms
+    with open(filename, 'w') as f:
+        yaml.dump(month_data, f)
+
+def add_month_metadata(months, month_data):
+    month_data['month_idx'] = months.index(month_data['month'])+1
+    month_data['prev_month'] = months[months.index(month_data['month'])-1]
+    month_data['days'] = monthrange(month_data['year'], month_data['month_idx'])[1]
+    return month_data
+
+
+def read_gmaps_key(filename):
     '''Reads a googlemaps key from the disk and returns a client'''
-    with open(file, 'r') as f:
+    with open(filename, 'r') as f:
         gmaps_key = f.readlines()[0]
     gmaps = googlemaps.Client(key=gmaps_key)  
     return gmaps
@@ -53,17 +92,122 @@ def get_route_distance(gmaps, route):
 def weekend_ride():
     choice = np.random.randint(2)
     if choice == 0:
-        return 'No ride', 0
+        return 'Keine', 0
     else:
         distance = np.random.randint(5, 24)
-        return 'private', distance
+        return 'Private', distance
 
-def add_entry(route, distance='', description=''):
+def add_entry(gmaps, route, distance='', description=''):
     """Returns a row for the table."""
-    if route == 'private' or route == 'no':
-        return (route + ' drive', distance, '')
-    distance = get_route_distance(route)
+    if route[0] == 'Keine':
+        return (route[0] + ' Fahrt', 0, '')
+    if route[0] == 'Private':
+        return (route[0] + ' Fahrt', route[1], '')
+    distance = get_route_distance(gmaps, route)
     route = [place['name'] for place in route]
-    route = ' -- '.join(route)
+    route = ' - '.join(route)
 
     return route, distance, description
+
+
+def create_route_list(gmaps, db, month_data):
+    '''Create the list of routes by taking into account exceptions.
+
+    Args:
+        gmaps: a googlemaps client
+        month_data: dictionary holding the data of the month
+
+    Returns:
+        route_list: a list of routes
+    '''
+    route_list = []
+    for day in range(1, month_data['days']+1):
+        if day in month_data['exceptions']:
+            route = month_data['exceptions'][day]
+            if route[0] == 'Keine':
+                route_list.append(add_entry(gmaps, route))
+            elif route[0] == 'Private':
+                route_list.append(add_entry(gmaps, route))
+            else:
+                route_list.append(add_entry(gmaps, [db[address] for address in route]))
+        else:
+            date = datetime.datetime(month_data['year'], month_data['month_idx'], day)
+            if date.weekday() >= 5:
+                route_list.append(add_entry(gmaps, weekend_ride()))
+            else:
+                route_list.append(add_entry(gmaps, [db['Haus'], db['Praxis'], db['Haus']]))
+    return route_list
+
+def calculate_kms(route_list):
+    '''Calculate and return the normal and private kms'''
+    private_kms = 0
+    normal_kms = 0
+    for entry in route_list:
+        if entry[0] == 'Private Fahrt':
+            private_kms += entry[1]
+        else:
+            normal_kms += entry[1]
+    return normal_kms, private_kms
+
+def write_sheet(route_list, month_data):
+    '''Create the excel sheet'''
+    normal_kms, private_kms = calculate_kms(route_list)
+
+    # Create the excel sheet
+    workbook = xlsxwriter.Workbook('out/Fahrbuch_' + str(month_data['year']) + '_'
+        + month_data['month'] +  '.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.set_landscape()
+    worksheet.set_margins(top=0.35, bottom=0.35, right=0.5, left=0.35)
+    worksheet.set_column(0, 0, 15)
+    worksheet.set_column(1, 1, 40)
+    worksheet.set_column(2, 2, 10)
+    worksheet.set_column(3, 3, 20)
+
+    # Format styles
+    bold = workbook.add_format({'bold': True})
+    bold_left = workbook.add_format({'bold': True, 'align' : 'left'})
+    bold_center = workbook.add_format({'bold': True, 'align' : 'center'})
+    bold_right = workbook.add_format({'bold': True, 'align' : 'right'})
+    center = workbook.add_format({'align' : 'center'})
+    red_text = workbook.add_format({'font_color' : 'red'})
+    red_text_center = workbook.add_format({'font_color' : 'red', 'align' : 'center'})
+    red_text_right = workbook.add_format({'font_color' : 'red', 'align' : 'right', 'bold' : 'True'})
+
+    # Write the header
+    worksheet.write('A1', month_data['month'] + ' ' + str(month_data['year']), bold_center)
+    worksheet.write('B1', 'Last mileage from ' + month_data['prev_month'], bold)
+    worksheet.write('C1', month_data['last_km_stand'], bold_center)
+    worksheet.write('A2', 'Date', bold_center)
+    worksheet.write('B2', 'Route', bold_left)
+    worksheet.write('C2', 'Km', bold_center)
+    worksheet.write('D2', 'Comments', bold)
+
+    # Write the entries of the month
+    for entry in range(len(route_list)):
+        worksheet.write('A' + str(3+entry), str(entry+1)+'/' 
+                        + str(month_data['month_idx']) 
+                        + '/' + str(month_data['year']), center)
+        if route_list[entry][0] == 'Private Fahrt':
+            worksheet.write('B' + str(3+entry), route_list[entry][0], red_text)
+            worksheet.write('C' + str(3+entry), route_list[entry][1], red_text_center)
+        else:
+            worksheet.write('B' + str(3+entry), route_list[entry][0])
+            worksheet.write('C' + str(3+entry), route_list[entry][1], center)
+        worksheet.write('D' + str(3+entry), route_list[entry][2])
+
+    # Write the footer
+    worksheet.write('B' + str(3+len(route_list)), 'Overall km:', bold_right)
+    worksheet.write('C' + str(3+len(route_list)), normal_kms + private_kms, center)
+    worksheet.write('B' + str(3+len(route_list)+1), 'of which private:', red_text_right)
+    worksheet.write('C' + str(3+len(route_list)+1), private_kms, red_text_center)
+    worksheet.write('B' + str(3+len(route_list)+2), 'Tax deductible:', bold_right)
+    worksheet.write('C' + str(3+len(route_list)+2), normal_kms, center)
+
+    workbook.close()
+
+    subprocess.call(['libreoffice', '--headless',
+        '--convert-to', 'pdf',
+        'out/Fahrbuch_' + str(month_data['year']) + '_'
+        + month_data['month'] +  '.xlsx',
+        '--outdir', './out/'])
--- mileage.py
+++ mileage.py
@@ -1,128 +1,18 @@
-#!/usr/bin/env python
-
 __author__ = "Nikos Karaiskos"
 __copyright__ = "MIT License, Copyright (c) 2007"
 __version__ = "0.2"
 
-from calendar import monthrange
-import datetime
-import json
 from _utils import *
-import yaml
-import xlsxwriter
 
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-          'August', 'September', 'October', 'November', 'December']
+month_data_file = 'data/example.yaml'
 
-db = json.load(open('addresses.json'))
+gmaps = read_gmaps_key('data/gmaps_key.txt')
+months = read_months('data/months_en.txt')
+month_data = read_month_data(month_data_file)
+month_data = add_month_metadata(months, month_data)
+db = read_database('data/addresses.json')
 
-exceptions = {9 : [db['home'], db['work'], db['home'], db['work'], db['home']],
-             17 : [db['extra_location'], db['extra_location2'], db['home']]}
+route_list = create_route_list(gmaps, db, month_data)
+write_sheet(route_list, month_data)
 
-# Enter basic information
-last_km_stand = 89768
-current_month = 'January'
-current_year = 2021
-month_idx = months.index(current_month)+1
-previous_month = months[months.index(current_month)-1]
-
-
-days = monthrange(current_year, month_idx)[1]
-
-
-with open('example.yaml', 'r') as stream:
-    try:
-        month_data = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-
-
-
-
-# fill the list "automatically"
-the_list = []
-for day in range(1, days+1):
-    if day in exeptions:
-        the_list.append(add_entry(exeptions[day]))
-    else:
-        date = datetime.datetime(current_year, day, month_idx)
-        if date.weekday() >= 5:
-            the_list.append(add_entry(weekend_ride()))
-        else:
-            the_list.append(add_entry([db['home'], db['work'], db['home']]))
-
-
-
-
-
-# # Enter the routes as a list
-# the_list = [add_entry('private', 12),
-#             add_entry([home, work, home]),
-#             add_entry([home, work, hbf, work, home], description='Picking up client'),
-#             add_entry('no', 0),
-#             add_entry('private', 2),
-#             add_entry('private', 16)]
-
-
-# Calculate the total numbers of kms
-private_kms = 0
-normal_kms = 0
-for entry in the_list:
-    if entry[0] == 'private drive':
-        private_kms += entry[1]
-    else:
-        normal_kms += entry[1]
-
-
-
-
-
-# Create the excel sheet
-workbook = xlsxwriter.Workbook('mileage_' + str(current_year) + '_' + 
-                                current_month +  '.xlsx')
-worksheet = workbook.add_worksheet()
-worksheet.set_column(0, 0, 15)
-worksheet.set_column(1, 1, 50)
-worksheet.set_column(2, 2, 10)
-worksheet.set_column(3, 3, 40)
-
-# Format styles
-bold = workbook.add_format({'bold': True})
-bold_center = workbook.add_format({'bold': True, 'align' : 'center'})
-bold_right = workbook.add_format({'bold': True, 'align' : 'right'})
-center = workbook.add_format({'align' : 'center'})
-red_text = workbook.add_format({'font_color' : 'red'})
-red_text_center = workbook.add_format({'font_color' : 'red', 'align' : 'center'})
-red_text_right = workbook.add_format({'font_color' : 'red', 'align' : 'right', 'bold' : 'True'})
-
-# Write the header
-worksheet.write('A1', current_month + ' ' + current_year, bold_center)
-worksheet.write('B1', 'Last mileage from ' + previous_month, bold)
-worksheet.write('C1', last_km_stand, bold_center)
-worksheet.write('A3', 'Date', bold_center)
-worksheet.write('B3', 'Route', bold_center)
-worksheet.write('C3', 'Km', bold_center)
-worksheet.write('D3', 'Comments', bold)
-
-# Write the entries of the month
-for entry in range(len(the_list)):
-    worksheet.write('A' + str(4+entry), str(entry+1)+'/' + str(month_idx) 
-                        + '/' + current_year, center)
-    if the_list[entry][0] == 'private drive':
-        worksheet.write('B' + str(4+entry), the_list[entry][0], red_text)
-        worksheet.write('C' + str(4+entry), the_list[entry][1], red_text_center)
-    else:
-        worksheet.write('B' + str(4+entry), the_list[entry][0])
-        worksheet.write('C' + str(4+entry), the_list[entry][1], center)
-    worksheet.write('D' + str(4+entry), the_list[entry][2])
-
-# Write the footer
-worksheet.write('B' + str(4+len(the_list)+1), 'Overall km:', bold_right)
-worksheet.write('C' + str(4+len(the_list)+1), normal_kms+private_kms, center)
-worksheet.write('B' + str(4+len(the_list)+2), 'of which private:', red_text_right)
-worksheet.write('C' + str(4+len(the_list)+2), private_kms, red_text_center)
-worksheet.write('B' + str(4+len(the_list)+3), 'Tax deductible:', bold_right)
-worksheet.write('C' + str(4+len(the_list)+3), normal_kms, center)
-
-workbook.close()
+write_month_data(route_list, month_data, month_data_file)
